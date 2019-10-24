ab1aa203ea40db441c2f67078c00161c681209eb
arronechen <1371236464@qq.com>
2019-10-24 10:53:58 +0800

    增加随机自动生成问题所需字符串函数

--- image_ball/tk_demo/time_test.py
+++ image_ball/tk_demo/time_test.py
@@ -3,11 +3,34 @@ from pygame import *
 import threading
 import time
 import os
+import random
 
-list=[2]
-for i in range(1,50):
-      list.append(60*i)
-print(list)
-print(len(list))
-print(1/60*60)
-print(list[-1])
+# count = 0
+# for i in range(0,100):
+#       num1 = random.randint(0, 50)
+#       num2 = random.randint(0, 50)
+#       p =  random.uniform(0,1)
+#       if p>0.5:
+#             sum = num1 +num2
+#             count = count+1
+#       else:
+#             sum = random.randint(50, 100)
+#       str = "{}+{}={}?".format(num1,num2,sum)
+#
+#       print("{}{}{}{}{}{}{}".format(p," ",num1," ",num2," ",sum))
+#       print (str)
+# print(count)
+
+def strrandom():
+      num1 = random.randint(0, 50)
+      num2 = random.randint(0, 50)
+      p = random.uniform(0, 1)
+      if p > 0.5:
+            sum = num1 + num2
+      else:
+            sum = random.randint(50, 100)
+      str = "{}+{}={}?".format(num1, num2, sum)
+      return str
+for i in range(0,10):
+      str = strrandom()
+      print(str)
--- image_ball/tk_window/demo_1015.py
+++ image_ball/tk_window/demo_1015.py
@@ -8,6 +8,8 @@ from PIL import ImageTk, Image
 pygame.init()
 size = width, height = 500, 330
 window = pygame.display.set_mode(size)
+
+
 def getfiles(Path):
     """获取图片文件名。"""
     files = os.listdir(Path)
@@ -19,7 +21,8 @@ def getfiles(Path):
 
 textlist = ['1+1=2?', '2+5=7?', '3+17=20?', '55-5=60?', '54-4=60?', '1+9=10?', '2+18=30?',
             '3+20=32?', '55-11=44?', '54-4=50?', '1+59=70?', '2+18=20?', '3+15=18?', '55+99=154?',
-            '54+16=70?', '1+56=57?', '2+56=59?', '3+57=70?', '55+33=88?', '54+16=80?','2+5=7?', '3+17=20?', '55-5=60?', '54-4=60?', '1+9=10?', '2+18=30?',
+            '54+16=70?', '1+56=57?', '2+56=59?', '3+57=70?', '55+33=88?', '54+16=80?', '2+5=7?', '3+17=20?', '55-5=60?',
+            '54-4=60?', '1+9=10?', '2+18=30?',
             '3+20=32?', '55-11=44?', '54-4=50?', '1+59=70?', '2+18=20?', '3+15=18?', '55+99=154?',
             '54+16=70?', '1+56=57?', '2+56=59?', '3+57=70?', '55+33=88?', '54+16=80?'
             ]
@@ -38,23 +41,27 @@ for i in range(0, num):
     imagebox.append(pygame.image.load(Path + '\\' + files[i]))
 for i in range(0, num - 10):
     imagebox2.append(pygame.image.load(Path2 + '\\' + files2[i]))
+
+
 def toc(t1):
     t = time.time()
     return (t - t1) * 1000
+
+
 num = 0
-count=1
-t2=0
+count = 1
+t2 = 0
 res = 0
 t1 = time.time()
 # list = [2,62,122,182,242,302,362,422,500,560,620,680,740,800,860,1300]
-list=[2]
-for i in range(1,51):
-      list.append(60*i)
+list = [2]
+for i in range(1, 51):
+    list.append(60 * i)
 while 1:
-    while t2 < 1000 / 60*count:
+    while t2 < 1000 / 60 * count:
         t2 = toc(t1)
-    count = count+1
-    if count==list[-1]:
+    count = count + 1
+    if count == list[-1]:
         sys.exit()
     for event in pygame.event.get():  # 获取用户当前所做动作的事件列表
         if event.type == pygame.QUIT:
@@ -63,13 +70,13 @@ while 1:
             # 检测按键是否是a或者left
             if event.key == K_LEFT or event.key == K_RIGHT:
                 print('right')
-                num=num+1
+                num = num + 1
     # print(t2）
 
     for i in range(0, len(list)):
         if count == list[i]:
             window.blit(imagebox[i], (0, 0))
-            res=i
+            res = i
 
             pygame.display.update()
             print(toc(t1))
@@ -82,7 +89,7 @@ while 1:
             print(toc(t1))
             print(count)
             break
-    if res!=-1:
+    if res != -1:
         window.blit(imagebox[res], (0, 0))
 
     window.blit(surface[num], (40, 120))
