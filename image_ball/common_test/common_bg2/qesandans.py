49a81d4568343b10853434978f2d96c3b008fab6
arronechen <1371236464@qq.com>
2020-10-22 17:16:38 +0800

    调整按钮

--- image_ball/common_test/common_bg2/qesandans.py
+++ image_ball/common_test/common_bg2/qesandans.py
@@ -70,12 +70,12 @@ class qesandans():
         #print(state)
         btn8 = Button(400+self.weight/3, 275+self.height/3, "NEXT", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack, btnFont, (255, 0, 0))
         #print(state)
-        btn1 = Button(0+self.weight/3, 0+self.height/3, "看到", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack1, btnFont, (255, 0, 0))
-        btn2 = Button(200+self.weight/3, 0+self.height/3, "未看到", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack2, btnFont, (255, 0, 0))
-        btn3 = Button(0+self.weight/3, 100+self.height/3, "蝴蝶", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack3, btnFont, (255, 0, 0))
-        btn4 = Button(125+self.weight/3, 100+self.height/3, "青蛙", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack4, btnFont, (255, 0, 0))
-        btn5 = Button(250+self.weight/3, 100+self.height/3, "苹果", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack5, btnFont, (255, 0, 0))
-        btn6 = Button(375+self.weight/3, 100+self.height/3, "猴子", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack6, btnFont, (255, 0, 0))
+        btn1 = Button(self.weight/3-50, 0+self.height/3, "看到不同栅距光栅", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack1, btnFont, (255, 0, 0))
+        btn2 = Button(350+self.weight/3, 0+self.height/3, "未看到不同栅距光栅", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack2, btnFont, (255, 0, 0))
+        #btn3 = Button(0+self.weight/3, 100+self.height/3, "蝴蝶", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack3, btnFont, (255, 0, 0))
+        #btn4 = Button(125+self.weight/3, 100+self.height/3, "青蛙", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack4, btnFont, (255, 0, 0))
+        #btn5 = Button(250+self.weight/3, 100+self.height/3, "苹果", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack5, btnFont, (255, 0, 0))
+        #btn6 = Button(375+self.weight/3, 100+self.height/3, "猴子", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack6, btnFont, (255, 0, 0))
         btn7 = Button(100+self.weight/3, 200+self.height/3, "不确定", surBtnNormal, surBtnMove, surBtnDown, self.btnCallBack7, btnFont, (255, 0, 0))
 
         #print("2333333333")
@@ -91,20 +91,20 @@ class qesandans():
                     # 判断鼠标是否移动到按钮范围内
                     btn1.getFocus(mx, my)
                     btn2.getFocus(mx, my)
-                    btn3.getFocus(mx, my)
-                    btn4.getFocus(mx, my)
-                    btn5.getFocus(mx, my)
-                    btn6.getFocus(mx, my)
+                    #btn3.getFocus(mx, my)
+                    #btn4.getFocus(mx, my)
+                    #btn5.getFocus(mx, my)
+                    #btn6.getFocus(mx, my)
                     btn7.getFocus(mx, my)
                     btn8.getFocus(mx, my)
                 elif event.type == pygame.MOUSEBUTTONDOWN:  # 鼠标按下
                     if pygame.mouse.get_pressed() == (1, 0, 0):  # 鼠标左键按下
                         btn1.mouseDown(mx, my)
                         btn2.mouseDown(mx, my)
-                        btn3.mouseDown(mx, my)
-                        btn4.mouseDown(mx, my)
-                        btn5.mouseDown(mx, my)
-                        btn6.mouseDown(mx, my)
+                        #btn3.mouseDown(mx, my)
+                        #btn4.mouseDown(mx, my)
+                        #btn5.mouseDown(mx, my)
+                        #btn6.mouseDown(mx, my)
                         btn7.mouseDown(mx, my)
                         btn8.mouseDown(mx, my)
                         # print("鼠标按下")
@@ -113,14 +113,14 @@ class qesandans():
                         tr1.set_see(btn1.mouseUp())
                     if (tr1.get_see() == None):
                         tr1.set_see(btn2.mouseUp())
-                    if (tr1.get_thing() == None):
-                        tr1.set_thing(btn3.mouseUp())
-                    if (tr1.get_thing() == None):
-                        tr1.set_thing(btn4.mouseUp())
-                    if (tr1.get_thing() == None):
-                        tr1.set_thing(btn5.mouseUp())
-                    if (tr1.get_thing() == None):
-                        tr1.set_thing(btn6.mouseUp())
+                    #if (tr1.get_thing() == None):
+                        #tr1.set_thing(btn3.mouseUp())
+                    #if (tr1.get_thing() == None):
+                        #tr1.set_thing(btn4.mouseUp())
+                    #if (tr1.get_thing() == None):
+                       # tr1.set_thing(btn5.mouseUp())
+                    #if (tr1.get_thing() == None):
+                        #tr1.set_thing(btn6.mouseUp())
                     if (tr1.get_thing() == None):
                         tr1.set_thing(btn7.mouseUp())
                     btn8.mouseUp3()
@@ -134,10 +134,10 @@ class qesandans():
             # 绘制按钮
             btn1.draw(window)
             btn2.draw(window)
-            btn3.draw(window)
-            btn4.draw(window)
-            btn5.draw(window)
-            btn6.draw(window)
+            #btn3.draw(window)
+            #btn4.draw(window)
+            #btn5.draw(window)
+            #btn6.draw(window)
             btn7.draw(window)
             btn8.draw(window)
             pygame.display.flip()
